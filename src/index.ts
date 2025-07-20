378924ab185e8609c2b92f080c623a284bcf36b9
nopaldonh <nopaldonh21@gmail.com>
2025-07-20 21:22:05 +0700

    extract hono app to own file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { serve } from "@hono/node-server";
-import { OpenAPIHono } from "@hono/zod-openapi";
 
-const app = new OpenAPIHono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+import app from "./app";
 
 serve({
   fetch: app.fetch,
   port: 3000,
 }, (info) => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${info.port}`);
 });
