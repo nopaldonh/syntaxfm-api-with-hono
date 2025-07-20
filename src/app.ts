773bb62af5857472382c9f1b3917569fb7812461
nopaldonh <nopaldonh21@gmail.com>
2025-07-20 21:59:01 +0700

    logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
 import { notFound, onError } from "stoker/middlewares";
 
 const app = new OpenAPIHono();
+
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
