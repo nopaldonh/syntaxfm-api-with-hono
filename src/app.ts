ba57cf5af60980702d9a6e333145a0bba035e469
nopaldonh <nopaldonh21@gmail.com>
2025-07-20 22:44:55 +0700

    hono-pino pretty logs and customization

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { requestId } from "hono/request-id";
 import { notFound, onError } from "stoker/middlewares";
 
 import { logger } from "./middlewares/pino-logger";
 
 const app = new OpenAPIHono();
 
+app.use(requestId({ generator: () => crypto.randomUUID() }));
 app.use(logger());
 
 app.get("/", (c) => {
--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,5 +1,12 @@
 import { pinoLogger } from "hono-pino";
+import pino from "pino";
+import pretty from "pino-pretty";
 
 export function logger() {
-  return pinoLogger();
+  return pinoLogger({
+    pino: pino(process.env.NODE_ENV === "production" ? undefined : pretty()),
+    // http: {
+    //   reqId: () => crypto.randomUUID(),
+    // },
+  });
 }
