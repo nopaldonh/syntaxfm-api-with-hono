4492f9557c0dcd2ed9b18396450fa0a159c974f1
nopaldonh <nopaldonh21@gmail.com>
2025-07-20 22:27:06 +0700

    structured logging with pino and hono-pino

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { logger } from "hono/logger";
 import { notFound, onError } from "stoker/middlewares";
+
+import { logger } from "./middlewares/pino-logger";
 
 const app = new OpenAPIHono();
 
