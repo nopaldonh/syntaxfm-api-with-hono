08d20bb03530ea95b7ed2d6333e9a171cb13d380
nopaldonh <nopaldonh21@gmail.com>
2025-07-20 23:12:19 +0700

    pino log level setting

--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -4,7 +4,9 @@ import pretty from "pino-pretty";
 
 export function logger() {
   return pinoLogger({
-    pino: pino(process.env.NODE_ENV === "production" ? undefined : pretty()),
+    pino: pino({
+      level: process.env.LOG_LEVEL || "info",
+    }, process.env.NODE_ENV === "production" ? undefined : pretty()),
     // http: {
     //   reqId: () => crypto.randomUUID(),
     // },
