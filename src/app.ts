85b17f0ed6ffc2f6b591a1db19f8901f8807fb27
nopaldonh <nopaldonh21@gmail.com>
2025-07-20 23:04:07 +0700

    typesafe hono AppBindings

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,30 @@
+import type { PinoLogger } from "hono-pino";
+
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { requestId } from "hono/request-id";
 import { notFound, onError } from "stoker/middlewares";
 
 import { logger } from "./middlewares/pino-logger";
 
-const app = new OpenAPIHono();
+interface AppBindings {
+  Variables: {
+    logger: PinoLogger;
+  };
+}
+
+const app = new OpenAPIHono<AppBindings>();
 
 app.use(requestId({ generator: () => crypto.randomUUID() }));
 app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+
+app.get("/error", (c) => {
+  c.status(422);
+  c.var.logger.info("Error log info.");
+  throw new Error("Oh No!");
 });
 
 app.notFound(notFound);
