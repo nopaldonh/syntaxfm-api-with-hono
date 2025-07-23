47c8a99e4c26e8bf406edaa4482a7d0080b0d314
nopaldonh <nopaldonh21@gmail.com>
2025-07-24 01:59:37 +0700

    OpenAPIHono defaultHook

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { requestId } from "hono/request-id";
 import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
+import { defaultHook } from "stoker/openapi";
 
 import { logger } from "@/middlewares/pino-logger";
 
@@ -9,6 +10,7 @@ import type { AppBindings } from "./types";
 export function createRouter() {
   return new OpenAPIHono<AppBindings>({
     strict: false,
+    defaultHook,
   });
 }
 
