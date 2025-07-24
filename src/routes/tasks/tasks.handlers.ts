cb4f96bedafa6447d85727890a47628553d7cd02
nopaldonh <nopaldonh21@gmail.com>
2025-07-24 11:34:12 +0700

    register tasks list route and handler

--- src/routes/tasks/tasks.handlers.ts
+++ src/routes/tasks/tasks.handlers.ts
@@ -1,6 +1,6 @@
 import type { AppRouteHandler } from "@/lib/types";
 
-import type { ListRoute } from "./tasks.route";
+import type { ListRoute } from "./tasks.routes";
 
 export const list: AppRouteHandler<ListRoute> = (c) => {
   return c.json([
--- src/routes/tasks/tasks.index.ts
+++ src/routes/tasks/tasks.index.ts
@@ -1,5 +1,9 @@
 import { createRouter } from "@/lib/create-app";
 
-const router = createRouter();
+import * as handlers from "./tasks.handlers";
+import * as routes from "./tasks.routes";
+
+const router = createRouter()
+  .openapi(routes.list, handlers.list);
 
 export default router;
