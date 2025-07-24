3ee10f35cfdd0b0ad44449c29c517b283003de07
nopaldonh <nopaldonh21@gmail.com>
2025-07-24 14:32:16 +0700

    query db from tasks list handler

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,15 @@ import { drizzle } from "drizzle-orm/libsql";
 
 import env from "@/env";
 
+import * as schema from "./schema";
+
 const client = createClient({
   url: env.DATABASE_URL,
   authToken: env.DATABASE_AUTH_TOKEN,
 });
 
-const db = drizzle(client);
+const db = drizzle(client, {
+  schema,
+});
 
 export default db;
