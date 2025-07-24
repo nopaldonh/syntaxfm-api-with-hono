483f3806382af12f7934f039e44a9fc2dbaa33a7
nopaldonh <nopaldonh21@gmail.com>
2025-07-24 11:49:13 +0700

    DATABASE_URL env setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,11 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
+import env from "@/env";
+
 const client = createClient({
-  url: process.env.DATABASE_URL,
-  authToken: process.env.DATABASE_AUTH_TOKEN,
+  url: env.DATABASE_URL,
+  authToken: env.DATABASE_AUTH_TOKEN,
 });
 
 const db = drizzle(client);
--- src/env.ts
+++ src/env.ts
@@ -10,6 +10,13 @@ const EnvSchema = z.object({
   NODE_ENV: z.string().default("development"),
   PORT: z.coerce.number().default(3000),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
+  DATABASE_URL: z.url(),
+  DATABASE_AUTH_TOKEN: z.string().optional(),
+}).refine((input) => {
+  if (input.NODE_ENV === "production") {
+    return !!input.DATABASE_AUTH_TOKEN;
+  }
+  return true;
 });
 
 export type env = z.infer<typeof EnvSchema>;
