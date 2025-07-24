41726b8ac0ab7413089f1f174674c9da7647d7d5
nopaldonh <nopaldonh21@gmail.com>
2025-07-24 11:15:59 +0700

    RouteHandler type setup

--- src/routes/tasks/tasks.handler.ts
+++ src/routes/tasks/tasks.handler.ts
@@ -1,8 +1,10 @@
 import type { RouteHandler } from "@hono/zod-openapi";
 
+import type { AppBindings } from "@/lib/types";
+
 import type { ListRoute } from "./tasks.route";
 
-export const list: RouteHandler<ListRoute> = (c) => {
+export const list: RouteHandler<ListRoute, AppBindings> = (c) => {
   return c.json([
     {
       name: "Learn Hono",
