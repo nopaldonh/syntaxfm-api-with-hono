2519a8df7321ac80a0db290240e7913d4c7d6efe
nopaldonh <nopaldonh21@gmail.com>
2025-07-24 11:35:34 +0700

    tasks route tags

--- src/routes/tasks/tasks.routes.ts
+++ src/routes/tasks/tasks.routes.ts
@@ -2,7 +2,10 @@ import { createRoute, z } from "@hono/zod-openapi";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
 
+const tags = ["Tasks"];
+
 export const list = createRoute({
+  tags,
   path: "/tasks",
   method: "get",
   responses: {
