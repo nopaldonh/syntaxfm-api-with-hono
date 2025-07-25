22d732b6f18e201e79a2450636c2b7b35f3b34e2
nopaldonh <nopaldonh21@gmail.com>
2025-07-25 23:44:47 +0700

    tasks remove route

--- src/routes/tasks/tasks.routes.ts
+++ src/routes/tasks/tasks.routes.ts
@@ -95,7 +95,30 @@ export const patch = createRoute({
   },
 });
 
+export const remove = createRoute({
+  path: "/tasks/{id}",
+  method: "delete",
+  request: {
+    params: IdParamsSchema,
+  },
+  tags,
+  responses: {
+    [HttpStatusCodes.NO_CONTENT]: {
+      description: "Task deleted",
+    },
+    [HttpStatusCodes.NOT_FOUND]: jsonContent(
+      notFoundSchema,
+      "Task not found",
+    ),
+    [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(IdParamsSchema),
+      "Invalid id error",
+    ),
+  },
+});
+
 export type ListRoute = typeof list;
 export type CreateRoute = typeof create;
 export type GetOneRoute = typeof getOne;
 export type PatchRoute = typeof patch;
+export type RemoveRoute = typeof remove;
